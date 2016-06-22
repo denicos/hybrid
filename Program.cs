05b0c1bde05362c4bef86c04f248f5d0bbaf81fe
denicos <nicoswaves.mn@gmail.com>
2016-06-22 10:57:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squareBracks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3};
+            Console.WriteLine(arr[0]);
+            string str ="Hello";
+            Console.WriteLine(str[1]);
+        }
+    }
+}
