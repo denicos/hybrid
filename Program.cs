9cf5925fe2db7bccf3b591c9014c9f6871263e77
denicos <nicoswaves.mn@gmail.com>
2016-06-28 15:03:21 +0300

    exe13.26

--- Program.cs
+++ Program.cs
@@ -3,17 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace squareBracks
+namespace exe13._26
 {
     class Program
     {
-        static void Main(string[] args)
+         private static string GetNonTagData(string input, out string title)
+    {
+        // first, extract the title of the HTML document
+        string pattern = @"<\s*title\s*>(?<title>[^<>]*)<\s*/\s*title\s*>";
+        Match titleMatch = Regex.Match(input, pattern);
+
+        title = String.Empty;
+
+        if (titleMatch.Success)
         {
-            int[] arr = {1,2,3};
-            Console.WriteLine(arr[0]);
-            string str ="Hello";
-            Console.WriteLine(str[1]);
+            title = titleMatch.Groups["title"].Value;
         }
+
+        string headPattern = @"<\s*head\s*>(.|\s)*?<\s*/\s*head\s*>";
+
+        // remove the <head> part of the document
+        string buffer = Regex.Replace(input, headPattern, String.Empty);
+
+        string bodyPattern = @">(?<content>[^<>]+)<";
+        MatchCollection bodyMatches = Regex.Matches(buffer, bodyPattern);
+
+        StringBuilder contentsBuilder = new StringBuilder();
+
+        foreach (Match bodyMatch in bodyMatches)
+        {
+            contentsBuilder.Append(bodyMatch.Groups["content"].Value.Trim() + " ");
+        }
+
+        return contentsBuilder.ToString();
+    }
+    static void Main()
+    {
+        string htmlFilePath;
+
+        do
+        {
+            Console.WriteLine("Enter the path of a local HTML file:");
+            htmlFilePath = Console.ReadLine();
+        }
+        while (!File.Exists(htmlFilePath) ||
+            Path.GetExtension(htmlFilePath) != ".html" &&
+            Path.GetExtension(htmlFilePath) != ".htm");
+
+        string htmlMarkup = File.ReadAllText(htmlFilePath);
+
+        string title;
+        string nonTagData = GetNonTagData(htmlMarkup, out title);
+
+        Console.WriteLine("Title: {0}", title);
+        Console.WriteLine("Body:\n{0}", nonTagData);
+    }
+
     }
 }
