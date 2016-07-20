04068be117f186c3abaf01fad52904a88d19f8e1
denicos <nicoswaves.mn@gmail.com>
2016-07-20 19:44:57 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,64 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Text.RegularExpressions;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
-namespace exe13._26
+namespace stockone
 {
     class Program
+
     {
-         private static string GetNonTagData(string input, out string title)
-    {
-        // first, extract the title of the HTML document
-        string pattern = @"<\s*title\s*>(?<title>[^<>]*)<\s*/\s*title\s*>";
-        Match titleMatch = Regex.Match(input, pattern);
-
-        title = String.Empty;
-
-        if (titleMatch.Success)
+        private static string connection = Convert.ToString( ConfigurationManager.ConnectionStrings["DefaultConnection"]);
+        static void Main(string[] args)
         {
-            title = titleMatch.Groups["title"].Value;
+            //creates a connection string 
+            DataSet CustomersDataSet = new DataSet();
+            SqlDataAdapter da;
+            SqlCommandBuilder cmdBuilder;
+            
+            using (SqlConnection conn = new SqlConnection())
+            {
+               // conn.ConnectionString = "Server=[DEV-PC]; Database=[console.nic];User Id=sa;Password=kagwa18$$;";
+                conn.ConnectionString = connection;
+                conn.Open();
+                //creats a command of inserting into a table in sql database.
+                Console.WriteLine("INSERT INTO command");
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO console_nic(name,descriptione,phoneNumber)VALUES(@0,@1,@2)", conn);
+                
+                // user name.
+                Console.WriteLine("enter your name : ");
+                string name = Console.ReadLine();
+                //enter user description
+                Console.WriteLine("Enter user description : ");
+                string description = Console.ReadLine();
+                //price to the bid.
+                Console.WriteLine("price of the bid : ");
+                int price = Convert.ToInt32(Console.ReadLine());
+                // discount of 10%
+                var discountInPercentage = 0.1;
+                double discount = price * discountInPercentage;
+
+                insertCommand.Parameters.Add(new SqlParameter("0", name));
+
+                insertCommand.Parameters.Add(new SqlParameter("1", description));
+                insertCommand.Parameters.Add(new SqlParameter("2", price));
+
+                Console.WriteLine("commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery());
+                Console.WriteLine("Done! press enter to move to the next step");
+                Console.ReadLine();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM console_nic", conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    Console.WriteLine("FirstColumn \t\tSecond Column \t\t  \tThird Column");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(string.Format("{0}  \t\t\t | {1}  \t\t\t | {2}  \t\t", reader[0], reader[1], reader[2]));
+                        Console.ReadLine();
+                    }
+                }
+
+                Console.WriteLine("enter (A) to add a record, or enter (V) to view a record or Enter (D) to delete or Enter (E) to edit");
+                ///now as for the input
+                string A = Console.ReadLine();
+                string V = Console.ReadLine();
+                string D = Console.ReadLine();
+                string E = Console.ReadLine();
+
+
+
+                
+            }  
         }
-
-        string headPattern = @"<\s*head\s*>(.|\s)*?<\s*/\s*head\s*>";
-
-        // remove the <head> part of the document
-        string buffer = Regex.Replace(input, headPattern, String.Empty);
-
-        string bodyPattern = @">(?<content>[^<>]+)<";
-        MatchCollection bodyMatches = Regex.Matches(buffer, bodyPattern);
-
-        StringBuilder contentsBuilder = new StringBuilder();
-
-        foreach (Match bodyMatch in bodyMatches)
-        {
-            contentsBuilder.Append(bodyMatch.Groups["content"].Value.Trim() + " ");
-        }
-
-        return contentsBuilder.ToString();
-    }
-    static void Main()
-    {
-        string htmlFilePath;
-
-        do
-        {
-            Console.WriteLine("Enter the path of a local HTML file:");
-            htmlFilePath = Console.ReadLine();
-        }
-        while (!File.Exists(htmlFilePath) ||
-            Path.GetExtension(htmlFilePath) != ".html" &&
-            Path.GetExtension(htmlFilePath) != ".htm");
-
-        string htmlMarkup = File.ReadAllText(htmlFilePath);
-
-        string title;
-        string nonTagData = GetNonTagData(htmlMarkup, out title);
-
-        Console.WriteLine("Title: {0}", title);
-        Console.WriteLine("Body:\n{0}", nonTagData);
-    }
-
     }
 }
